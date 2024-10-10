622a5dfcf7f84134d10aa41c3c6a24030c0ac440
Alexander Löhn <alexander.loehn.ext@scayle.com>
2024-10-10 19:19:14 +0200

    rename

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/interval-timer/",
+  base: "/nn-timer/",
   publicDir: "src/assets",
 });
